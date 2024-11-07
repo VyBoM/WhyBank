75320881e6e4dbdf604306ee146c87c8b299abdc
VyBoM <slava2006.150@gmail.com>
2024-11-08 02:07:34 +1000

    Ready full!

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,6 +4,7 @@ import styles from './MainPage.module.scss';
 
 import panorama from '../../assets/img/Panorama.png';
 import panoramaCard from '../../assets/img/Panorama-card.png';
+import panoramaCardMobile from '../../assets/img/Panorama-card-mobile.png';
 
 export const MainPage = () => {
 	return (
@@ -14,13 +15,14 @@ export const MainPage = () => {
 					<Link to='https://www.utorrent.com/intl/ru/downloads/win/'>
 						<img src={panorama} alt='Панорама' className={styles.panorama} />
 					</Link>
-					<div className={styles.panorama_card}>
+					<div className={styles.panorama_card_div}>
 						<img src={panoramaCard} alt='Панорама' className={styles.panorama_card} />
+						<img src={panoramaCardMobile} alt='Панорама' className={styles.panorama_card_mobile} />
 						<Link to='/card.html' className={styles.button_main}>
 							Оформить карту
 						</Link>
 					</div>
-					<h1 className={styles.h2}>С нашей картой ты не пропадешь</h1>
+					<h1 className={styles.h2}>ЛЮБЛЮ ТЕБЯ НАДЯ!❤️</h1>
 				</div>
 			</main>
 		</>
